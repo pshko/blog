3563416fce6b96b8ff50aeb618c0151318fbc3b7
Your Name <aram.khorsandi@gmail.com>
2021-02-08 17:26:49 +0330

    upload

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,42 @@
+<x-app-layout>
+    <x-slot name="title">
+        صفحه ورود
+    </x-slot>
+<main class="bg--white">
+    <div class="container">
+        <div class="sign-page">
+            <h1 class="sign-page__title">ورود به وب‌سایت</h1>
+            <form class="sign-page__form" method="POST" action="{{ route('login.store') }}">
+                @csrf
+                    <input type="text" name="email" class="text text--left" placeholder="شماره یا ایمیل">
+                    @error('email')
+                    <p style="text-align: right;
+                              margin-bottom: 1rem;
+                              color: #D8000C;">
+                              {{ $message }}
+                             </p>
+                @enderror
+                    <input type="password" name="password" class="text text--left" placeholder="رمز عبور">
+                    @error('password')
+                    <p style="text-align: right;
+                              margin-bottom: 1rem;
+                              color: #D8000C;">
+                              {{ $message }}
+                             </p>
+                @enderror
+                    <label class="checkbox text--right">
+                        <input type="checkbox" name="remember" class="checkbox__filter">
+                        <span class="checkbox__mark position-relative"></span>
+                        مرا بخاطر بسپار
+                    </label>
+                    <a class="recover-password" href="{{ route('password.request') }}">بازیابی رمز عبور</a>
+                    <button class="btn btn--blue btn--shadow-blue width-100 " type="submit">ورود به سایت</button>
+                    <div class="sign-page__footer">
+                        <span>کاربر جدید هستید؟</span>
+                        <a href="{{ route('register') }}" class="color--46b2f0">صفحه ثبت نام</a>
+                    </div>
+            </form>
+        </div>
+    </div>
+</main>
+</x-app-layout>
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,65 @@
+<x-app-layout>
+    <x-slot name="title">
+        ثبت نام
+    </x-slot>
+<main class="bg--white">
+    <div class="container">
+        <div class="sign-page">
+            <h1 class="sign-page__title">ثبت نام در وب‌سایت</h1>
+
+            
+            <form class="sign-page__form" action="{{ route('register.store') }}" method="POST">
+                @CSRF
+                <div>
+                    <input type="text" class="text text--right" name="name" placeholder="نام  و نام خانوادگی">
+                    @error('name')
+                        <p style="text-align: right;
+                                  margin-bottom: 1rem;
+                                  color: #D8000C;">
+                                  {{ $message }}
+                                 </p>
+                    @enderror
+                </div>
+                <div>
+                    <input type="text" class="text text--left" name="mobile" placeholder="شماره موبایل">
+                    @error('mobile')
+                    <p style="text-align: right;
+                              margin-bottom: 1rem;
+                              color: #D8000C;">
+                              {{ $message }}
+                             </p>
+                @enderror
+                </div>
+                <div>
+                    <input type="email" class="text text--left" name="email" placeholder="ایمیل">
+                    @error('email')
+                    <p style="text-align: right;
+                              margin-bottom: 1rem;
+                              color: #D8000C;">
+                              {{ $message }}
+                             </p>
+                @enderror
+                </div>
+                <div>
+                    <input type="password" class="text text--left" name="password" placeholder="رمز عبور">
+                    @error('password')
+                    <p style="text-align: right;
+                              margin-bottom: 1rem;
+                              color: #D8000C;">
+                              {{ $message }}
+                             </p>
+                @enderror
+                </div>
+                <div>
+                    <input type="password" class="text text--left" name="password_confirmation" placeholder="تکرار رمز عبور">
+                </div>
+                    <button class="btn btn--blue btn--shadow-blue width-100 mb-10" type="submit">ثبت نام</button>
+                    <div class="sign-page__footer">
+                        <span>در سایت عضوید ؟ </span>
+                        <a href="{{ route('login') }}" class="color--46b2f0">صفحه ورود</a>
+                    </div>
+            </form>
+        </div>
+    </div>
+</main>
+</x-app-layout>
