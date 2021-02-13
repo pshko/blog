fc365aea6da5f886df05032e232c4739683b0f5c
pshko <aram.khorsandi@gmail.com>
2021-02-13 15:51:22 +0330

    refactor categories controller

--- app/Http/Controllers/Panel/CategoryController.php
+++ app/Http/Controllers/Panel/CategoryController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Panel;
 use App\Http\Controllers\Controller;
 
+use App\Http\Requests\Panel\Category\CategoryCreateRequest;
+use App\Http\Requests\Panel\Category\CategoryUpdateRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -11,7 +13,7 @@ class CategoryController extends Controller
 
     public function index()
     {
-        $categories = Category::paginate(4);
+        $categories = Category::with('parent')->paginate(4);
         $ParentCategories = Category::where('category_id', NULL)->get();
         return view('panel.categories.index', compact('categories', 'ParentCategories'));
     }
@@ -23,15 +25,11 @@ class CategoryController extends Controller
     }
 
 
-    public function store(Request $request)
+    public function store(CategoryCreateRequest $request)
     {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'slug' => ['required', 'string', 'max:255', 'unique:categories'],
-            'category_id' => ['nullable', 'exists:categories,id']
-        ]);
+
             Category::create(
-                $request->only(['name', 'slug', 'category_id'])
+                $request->validated()
             );
             session()->flash('status', 'دسته بندی مورد نظر به درستی ایجاد شد!');
             return back();
@@ -49,14 +47,10 @@ class CategoryController extends Controller
         return view('panel.categories.edit', compact('category', 'ParentCategories'));
     }
 
-    public function update(Request $request, Category $category)
+    public function update(CategoryUpdateRequest $request, Category $category)
     {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'category_id' => ['nullable', 'exists:categories,id']
-        ]);
         $category->update(
-          $request->only('name', 'category_id')
+          $request->validated()
         );
         session()->flash('status', 'دسته بندی به درستی ویرایش شد!');
         return redirect()->route('categories.index');
